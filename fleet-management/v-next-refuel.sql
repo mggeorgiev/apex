835bd9c782bc130fc210c018b5227bbac47b5044
mggeorgiev <mggeorgiev@gmail.com>
2019-04-13 22:04:45 +0300

    update next refuel view

--- fleet-management/v-next-refuel.sql
+++ fleet-management/v-next-refuel.sql
@@ -1,10 +1,37 @@
-CREATE OR REPLACE FORCE VIEW "CARS"."NEXT_REFUEL" ("ID_CAR", "DKN", "PREVIOUS_DATE", "PREDICTED_DATE", "EXPECTED_PERIOD", "KLM", "KM", "PREDICTED_KM") AS 
-	SELECT 
-     WHERE DATE_COL > SYSDATE-120
-     AND PERIOD != 0
-     GROUP BY ID_CAR) prdm
-    ON prdm.ID_CAR = mx.ID_CAR            
-   JOIN CARS ON mx.ID_CAR = CARS.ID_CAR        
-  WHERE mx.MAX_KM = kms.KM        
- AND mx.ID_CAR = kms.ID_CAR        
-AND CARS.ACTIVE=1;
+CREATE OR REPLACE FORCE VIEW  "NEXT_REFUEL" ("ID_CAR", "DKN", "KM", "PREVIOUS_DATE", "PREDICTED_DATE", "EXPECTED_DATE") AS 
+  SELECT
+        DISTINCT mx.ID_CAR,
+        DKN,
+        MAX_KM AS KM,
+        MAX_DATE AS PREVIOUS_DATE,
+        MAX_DATE+ROUND(AMOUNT/PER_DIEM,0) AS PREDICTED_DATE,
+        ROUND(AMOUNT/PER_DIEM,0) AS EXPECTED_DATE
+        FROM
+            (SELECT 
+                ID_CAR,
+                MAX(KM) as MAX_KM,
+                MAX(DATE_COL) as MAX_DATE
+                FROM FUEL
+                GROUP BY ID_CAR) mx
+            JOIN
+            (SELECT 
+                ID_CAR,
+                KM,
+                AMOUNT
+                FROM FUEL) kms
+            ON mx.ID_CAR = kms.ID_CAR
+            JOIN
+            (SELECT 
+                ID_CAR,
+                ROUND(SUM(AMOUNT)/SUM(PERIOD),2) as PER_DIEM
+                FROM FUEL
+                WHERE DATE_COL > SYSDATE-120
+                AND PERIOD != 0
+                GROUP BY ID_CAR
+            ) prdm 
+            ON prdm.ID_CAR = mx.ID_CAR
+            JOIN CARS ON mx.ID_CAR = CARS.ID_CAR
+        WHERE mx.MAX_KM = kms.KM
+        AND mx.ID_CAR = kms.ID_CAR
+        AND CARS.ACTIVE=1
+/
