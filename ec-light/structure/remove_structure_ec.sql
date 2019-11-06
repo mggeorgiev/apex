378079baad49ca37c78914a7d250e86bfc0d8dd1
Martin Georgiev <mggeorgiev@gmail.com>
2019-11-06 21:26:44 +0200

    Create ec-lite

---
+++ ec-light/structure/remove_structure_ec.sql
@@ -0,0 +1,54 @@
+drop TABLE "ENTRANCE" cascade constraints
+/   
+DROP TABLE  "HA" cascade constraints
+/
+DROP TABLE  "BUILDING" cascade constraints
+/
+drop TABLE "APARTMENTS" cascade constraints
+/
+DROP TABLE  "EXPENSE_50" cascade constraints
+/
+DROP TABLE  "ART50" cascade constraints
+/
+DROP TABLE  "CALC_METHOD" cascade constraints
+/
+DROP TABLE  "EXPENSE_TYPE" cascade constraints
+/
+DROP TABLE  "EXPENSES" cascade constraints
+/
+DROP TABLE  "SUPPLIER" cascade constraints
+/
+DROP TABLE  "EXPENSES_LOG" cascade constraints
+/
+DROP TABLE  "EXPENSES_PRE" cascade constraints
+/
+DROP TABLE  "ROLES" cascade constraints
+/
+DROP TABLE  "HABITANTS" cascade constraints
+/
+DROP TABLE  "HOUSEHOLDS" cascade constraints
+/
+DROP TABLE  "INCOME_LOG" cascade constraints
+/
+DROP TABLE  "INCOME_LOG_PRE" cascade constraints
+/
+DROP TABLE  "NMW" cascade constraints
+/
+drop VIEW "EC_EXPENSES_SORTED"
+/ 
+drop SEQUENCE "ART50_SEQ"
+/   
+drop SEQUENCE "DEMO_USERS_SEQ"
+/   
+drop SEQUENCE "EXPENSES_PRE_SEQ"
+/   
+drop SEQUENCE "HA_SEQ"
+/   
+drop SEQUENCE "HOUSEHOLDS_SEQ"
+/   
+drop SEQUENCE "INCOME_LOG_PRE_SEQ"
+/   
+drop SEQUENCE "INCOME_LOG_SEQ"
+/   
+drop SEQUENCE "NMW_SEQ"
+/   
